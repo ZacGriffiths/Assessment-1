2197ae56534c84c9eacf715ccdd5a342e29b3029
Zac Griffiths <c3324597@uon.edu.au>
2019-04-02 01:35:15 +0000

    new code

--- main.c
+++ main.c
@@ -2,5 +2,18 @@
 
 int main(void)
 {
-    printf("hi\n");
+char c;
+ /* This will be the user interface*/
+do {
+printf("Please enter a letter that correspondes to the function you wish to use\n");
+printf("a) rotation cipher encryption\n");
+printf("b) rotation cipher decryption using the rotation amount\n");
+printf("c) rotation cipher decryption (no help)\n");
+printf("d) substitution cipher encryption\n");
+printf("e) substitution cipher decryption with given substitutions\n");   
+printf("f) substitution cipher decryption (no help)\n\n");
+printf("Selection: ");
+scanf(%c, &c);
+}
+while ()
 }
