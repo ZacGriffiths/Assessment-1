3519349eca1c3ca8b2ccc706766dab0674cab6a9
Zac Griffiths <c3324597@uon.edu.au>
2019-04-02 02:29:28 +0000

    Added a do while loop for the user interface that is not completed yet. It runs but does not allow user input

--- main.c
+++ main.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 
-int main(void)
-{
-char c;
+int main(void){
+
  /* This will be the user interface*/
-do {
-printf("Please enter a letter that correspondes to the function you wish to use\n");
-printf("a) rotation cipher encryption\n");
-printf("b) rotation cipher decryption using the rotation amount\n");
-printf("c) rotation cipher decryption (no help)\n");
-printf("d) substitution cipher encryption\n");
-printf("e) substitution cipher decryption with given substitutions\n");   
-printf("f) substitution cipher decryption (no help)\n\n");
-printf("Selection: ");
-scanf(%c, &c);
-<<<<<<< HEAD
-//add 4
-=======
-//add 32165165
->>>>>>> refs/heads/master
-}
-while ()
+char c;
+  do {
+     printf("Please enter a letter that correspondes to the function you wish to use\n");
+     printf("a) rotation cipher encryption\n");
+     printf("b) rotation cipher decryption using the rotation amount\n");
+     printf("c) rotation cipher decryption (no help)\n");
+     printf("d) substitution cipher encryption\n");
+     printf("e) substitution cipher decryption with given substitutions\n");   
+     printf("f) substitution cipher decryption (no help)\n\n");
+     printf("Selection: ");
+     scanf(" %c", &c);
+     }
+     while(c < 'a' || c > 'f');          /*the do loop is repeated until one of the letters is inputed*/
+      
+      switch(c)
+      {
+          case 'a':
+          printf("a");              /*this printf is just a place holder, input the function later*/
+          break;
+          
+          case 'b':
+          printf("b");             /*same here*/
+          break;
+          
+          case 'c':
+          printf("c");             /* and here*/
+          break;
+          
+          case 'd':
+          printf("d");             /* and here*/
+          break;
+          
+          case 'e':
+          printf("e");             /* same again*/
+          break;
+          
+          case 'f':
+          printf("f");             /* same again*/
+          break;
+          
+          default:
+          printf("That inputed character is not allocated to one of the function, please select another character");
+      }
 }
 
 
