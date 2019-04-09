e00e6d8ed651cfdb982bbbb4377c893cef7c057b
Zac Griffiths <c3324597@uon.edu.au>
2019-04-09 03:51:20 +0000

    End of lab 2
    I have implemented the rotation cipher but it currently only works for 1 word and not whitespace

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-
- /* This will be the user interface*/
+char c;
+ /* This is the user interface. It allows the user to pick what type of encryption or decryption they wish to use*/
      do
      {
-     char c;
      printf("\nPlease enter a letter that correspondes to the function you wish to use\n");
      printf("a) rotation cipher encryption\n");
      printf("b) rotation cipher decryption using the rotation amount\n");
@@ -21,42 +20,71 @@ int main(void){
     {}
      switch(c)
       {
-/************************************************************************************************************************************************************************/
+/************************************************************************/       
           case 'a':
-          printf("You have selected 'a'\n");              /*this printf is just a place holder, input the function later*/
+     {
+            char str[100];
+            int c = 0;
+            int n;
+            printf("Enter the word you wish to encrypt: ");
+            scanf("%s", str);
+            printf("Enter the number of rotations you want (any number over 26 will just loop over e.g. entering 29 will revert to 3: ");
+            scanf("%d", &n);
+            if (n > 26)
+                {
+                        n = n % 26;
+                }
+    
+            while (str[c] != '\0') 
+        {
+            if (str[c] >= 'a' && str[c] <= 'z')
+                {
+                        str[c] = str[c] - 32;
+                }
+            str[c] = str[c] - n;
+      
+            if (str[c] < 65)
+                {
+                        str[c] = str[c] + 26;
+                }
+            c++;
+        }
+          printf("\n%s\n", str);
           break;
+      }
+
           
-/************************************************************************************************************************************************************************/
+/************************************************************************/
           
           case 'b':
           printf("You have selected 'b'\n");             /*same here*/
           break;
           
-/************************************************************************************************************************************************************************/          
+/************************************************************************/          
           
           case 'c':
           printf("You have selected 'c'\n");             /* and here*/
           break;
           
-/************************************************************************************************************************************************************************/          
+/************************************************************************/             
           
           case 'd':
           printf("You have selected 'd'\n");             /* and here*/
           break;
           
-/************************************************************************************************************************************************************************/          
+/************************************************************************/         
           
           case 'e':
           printf("You have selected 'e'\n");             /* same again*/
           break;
           
-/************************************************************************************************************************************************************************/          
+/************************************************************************/                
           
           case 'f':
           printf("You have selected 'f'\n");             /* same again*/
           break;
           
-/************************************************************************************************************************************************************************/          
+/************************************************************************/                 
           
           default:
           printf("That inputed character is not allocated to one of the functions, please select another character\n");
