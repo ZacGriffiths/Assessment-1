5e6cf9e6dd706ddbdf706f5e2b5a83eb1ec53ed5
Zac Griffiths <c3324597@uon.edu.au>
2019-04-10 23:42:14 +0000

    I took the rotation cipher and included it in myfunctions.h therefore having it removed as a .c file.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "myfunctions.h"
 
 int main(void){
 char c;
@@ -22,37 +23,16 @@ char c;
       {
 /************************************************************************/       
           case 'a':
-     {
+       {
             char str[100];
-            int c = 0;
             int n;
             printf("Enter the word you wish to encrypt: ");
-            scanf("%s", str);
+            scanf("%[^\n]s", str);
             printf("Enter the number of rotations you want (any number over 26 will just loop over e.g. entering 29 will revert to 3: ");
             scanf("%d", &n);
-            if (n > 26)
-                {
-                        n = n % 26;
-                }
-    
-            while (str[c] != '\0') 
-        {
-            if (str[c] >= 'a' && str[c] <= 'z')
-                {
-                        str[c] = str[c] - 32;
-                }
-            str[c] = str[c] - n;
-      
-            if (str[c] < 65)
-                {
-                        str[c] = str[c] + 26;
-                }
-            c++;
-        }
-          printf("\n%s\n", str);
-          break;
-      }
-
+            RotationEncrypt(str, n);
+break;
+}
           
 /************************************************************************/
           
