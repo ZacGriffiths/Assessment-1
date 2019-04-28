eeca9eb98ae4ba821b5db6a3d9fb945a150c404b
Zac Griffiths <c3324597@uon.edu.au>
2019-04-28 23:43:43 +0000

    Stuff dosen't wanna work now, typical.

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/****************************************************************
+ *   Hello and welcome to my code.
+ *   Before running code you can place your phrase or word into
+ *   input.txt or input through stdin.
+ *   If using the substitution cipher, you can input your new alphabet/key
+ *   through the newalphabet.txt file or through stdin.
+ *   The inputed, alphabet(if using substitute cipher) and outputed code
+ *   will be displayed in the output.txt file.
+ *   The outputed code will also be displayed through stdout.
+ * 
+ *   Enjoy :)
+ * ***************************************************************/
+
 #include <stdio.h>
 #include "myfunctions.h" /* This is my functions for the encryption and decryption ciphers */
 #include <string.h>
@@ -7,16 +20,14 @@ FILE *output, *input, *newalphabet;
 char c; /* this is used to store the users input value */
 int i;  /* used for character identifying in the File I/O of functions*/
 int j;  /* another temporary character used in File I/O */
-char characters; /* used as a temporary value for characters in File I/O */
+char characters; /*used as a temporary value for characters in File I/O*/
 input = fopen("input.txt", "r+"); /* used as the input file for the messege to code or coded messege */
 output = fopen("output.txt", "w"); /* output file to show outputed decrypted or encrypted code */
-newalphabet = fopen("newalphabet.txt", "r+");
-
+newalphabet = fopen("newalphabet.txt", "r+"); /* This is the input file used for the newalphabet in substitution cipher*/
 /* This is the user interface. It allows the user to pick what type of 
 encryption or decryption they wish to use. These printf statements print
 the user interface while the scanf scans a character that the user has 
 inputed and stores it as a character 'c'*/
-
      do
      {
      printf("\nPlease enter a letter that correspondes to the function you wish to use\n");
@@ -38,19 +49,25 @@ inputed and stores it as a character 'c'*/
     greater then 'f' (any other number or letter) it will print the do loop (all
     the printf statements) and print the wrong character line until the user
     inputs a value of either 'a, b, c, d, e or f' */
-   
-     while(c < 'a' || c > 'f');
+    while(c < 'a' || c > 'f');
     {}
     /* If a character between a and f are inputed then the code then proceeds to
     this switch statement where the character inputed causes a cipher code to proceed */
      switch(c)
       {
-/************************************************************************/       
+/************************************************************************
+ * This is the code for the rotation cipher encryption. It takes the users phrase and number of
+ * rotations/key to output a new phrase.
+ **************************************************************************/
+
           case 'a':
        {
-            char str[1000];
-            int n;
-            fprintf(output, "Inputed phrase:\n\n");
+            char str[1000];/* The original and new string will be stored in here*/
+            int n;/* key */
+            fprintf(output, "Inputed phrase:\n\n");/*prints "inputed phrase" to file output */
+            /* this for loop takes the characters in the input.txt file, reads all of them until it reaches
+            the end of the file and prints out all charcters to the output.txt file under "inputed phrase".
+            The characters = 0 line is so there is no offset by 1 error*/
             for(i = 0; feof(input) == 0; i++)
             {
                 fscanf(input, "%c", &characters);
@@ -58,28 +75,37 @@ inputed and stores it as a character 'c'*/
                 str[i] = characters;
                 characters = 0;
            }
+           /*This if string will consider if there is any characters in the input.txt file and of not will ask
+           the user to input the phrase through stdin and subsequently printf that to output.txt*/
            if(str[0] == 0)
            {
                printf("Enter the phrase you wish to encrypt: ");
                scanf(" %[^\n]s", str);
                fprintf(output, "%s", str);
            }
+           /* printf and scanf used to identify the key*/
             printf("Enter the number of rotations/key you want (any number over 26 will just loop over e.g. entering 29 will revert to 3: ");
             scanf("%d", &n);
-            RotationEncrypt(str, n);
-            fprintf(output, "\n\nOutputed code: \n\n%s", str);
-            printf("your text is outputed in the output.txt file\n\n");
-            fopen("input.txt", "w+");
+            RotationEncrypt(str, n);/* The main code used for rotation cipher encryption */
+            fprintf(output, "\n\nOutputed code: \n\n%s", str); /*prints 'Outputed code' to output.txt*/
+            printf("your text is outputed in the output.txt file\n\n");/*Prints to console*/
+            fopen("input.txt", "w+"); /*Clears the input.txt file*/
+            printf("\n\nOutputed code: %s\n", str); /*prints new code to stdout*/
             break;
      }
           
-/************************************************************************/
+/************************************************************************
+ * Code used for rotation cipher decryption. It takes the users phrase and the key to decrypt the code
+ * and print the output
+ ***********************************************************************/
           
           case 'b':
           {
+            /* This code is similar to that used in the rotation cipher encryption*/
             char str[1000];
             int n;
             fprintf(output, "Inputed phrase:\n\n");
+            
             for(i = 0; feof(input) == 0; i++)
             {
                 fscanf(input, "%c", &characters);
@@ -95,19 +121,23 @@ inputed and stores it as a character 'c'*/
            }
             printf("Enter the number of rotations/key you want (any number over 26 will just loop over e.g. entering 29 will revert to 3: ");
             scanf("%d", &n);
-            RotationKeyDecryption(str, n);
+            RotationKeyDecryption(str, n); /*main code used for rotation cipher decryption(with key)*/
             fprintf(output, "\n\nOutputed code: \n\n%s", str);
             printf("your text is outputed in the output.txt file\n\n");
             fopen("input.txt", "w+");
+            printf("\n\nOutputed code: %s\n", str);
             break;
           }    
-/************************************************************************/          
+/************************************************************************
+ * This is the code for substitution cipher encryption. It takes the users phrase and the new alphabet
+ * to output the users encrypted code
+ ************************************************************************/
           
           case 'c':
           {
-              char str[1000];
-              char key[1000];
-              char newalphabetcharacters;
+              char str[1000]; /*users phrase*/
+              char key[1000]; /*The new alphabet*/
+              char newalphabetcharacters; /*temporary value for new alphabet*/
               fprintf(output, "Inputed phrase:\n\n");
               for(i = 0; feof(input) == 0; i++)
               {
@@ -123,6 +153,7 @@ inputed and stores it as a character 'c'*/
                   fprintf(output, "%s", str);
               }
               fprintf(output, "\n\nThe new alphabet:\n\n");
+              /*A similar code used for recognising the new alphabet*/
               for(j = 0; feof(newalphabet) == 0; j++)
               {
                   fscanf(newalphabet, "%c", &newalphabetcharacters);
@@ -135,19 +166,25 @@ inputed and stores it as a character 'c'*/
                  printf("Please enter the key (the new alphabet): ");
                  scanf("%s", key);
               }
-              SubstitutionCipherEncryption(str, key);
+              fprintf(output, "%s", key);
+              SubstitutionCipherEncryption(str, key); /*main code used for substitution cipher encryption*/
               fprintf(output, "\n\nOutputed code: \n\n%s", str);
               printf("your text is outputed in the output.txt file\n\n");
-              fopen("input.txt", "w+");
-              fopen("newalphabet.txt", "w+");
+              printf("\n\nOutputed code: %s\n", str);
+              fopen("input.txt", "w+"); /*Clears input.txt*/
+              fopen("newalphabet.txt", "w+"); /*Clears newalphabet.txt*/
           break;
         }
 
           
-/************************************************************************/             
+/************************************************************************
+ * The is the code used for substitution cipher decryption (with key). It takes the users phrase and the new
+ * alphabet to output the users decrypted code.
+ ***********************************************************************/
           
           case 'd':
           {
+              /*Code is similar to the substitution cipher encryption*/
               char str[1000];
               char key[1000];
               char newalphabetcharacters;
@@ -178,9 +215,11 @@ inputed and stores it as a character 'c'*/
                  printf("Please enter the key (the new alphabet): ");
                  scanf("%s", key);
               }
-              SubstitutionCipherDecryption(str, key);
+              fprintf(output, "%s", key);
+              SubstitutionCipherDecryption(str, key); /*main code used for substitution cipher decryption*/
               fprintf(output, "\n\nOutputed code: \n\n%s", str);
               printf("your text is outputed in the output.txt file\n\n");
+              printf("\n\nOutputed code: %s\n", str);
               fopen("input.txt", "w+");
               fopen("newalphabet.txt", "w+");
           break;
--- myfunctions.h
+++ myfunctions.h
@@ -1,28 +1,34 @@
 void RotationEncrypt(char str[], int n)
 {
     int c = 0;
+                /*If statement calculates key if the number is over 26*/
                 if (n > 26)
                 {
                         n = n % 26;
                 }
-
+/*This code will read till the end of the string(inputed phrase), convert all lower case letters to
+upper case, take those upper case letters and change them to a new letter based on the key number. If the 
+new letters are under the ascii number of 65(out of alphabet and into random symbols) then then code will convert
+them back to the beginning of the alphabet*/
+            /*reads till end of string*/
             while (str[c] != '\0') 
         {
+            /*converts lower case to upper case*/
             if (str[c] >= 'a' && str[c] <= 'z')
                 {
                         str[c] = str[c] - 32;
                 }
-
+            /*does calculation with key*/
             if (str[c] >= 'A' && str[c] <= 'Z')
             {
                 str[c] = str[c] - n;
-                
+                /*converts back to start of alphabet if under 65*/
                 if (str[c] > 32 && str[c] < 65)
                 {
                         str[c] = str[c] + 26;
                 }
             }     
-            c++;
+            c++; /*used to change to next position of string (eg str[0] to str[1])*/
         }
     return;
 }
@@ -31,24 +37,27 @@ void RotationEncrypt(char str[], int n)
 
 void RotationKeyDecryption (char str[], int n)
 {
+    /*Pretty much same again*/
     int c = 0;
+                /*Calculates key over 26*/
                 if (n > 26)
                 {
                         n = n % 26;
                 }
-
+            /*Reads till string finishes*/
             while (str[c] != '\0') 
         {
+            /*Converts to uppercase*/
             if (str[c] >= 'a' && str[c] <= 'z')
                 {
                         str[c] = str[c] - 32;
                 }
-
+            /*Does the calculation with the key*/
             if (str[c] >= 'A' && str[c] <= 'Z')
             {
                 str[c] = str[c] + n;
-                
-                if (str[c] > 32 && str[c] < 65)
+                /*If new letter is over the uppercase letter number then alphabet starts again*/
+                if (str[c] > 90)
                 {
                         str[c] = str[c] - 26;
                 }
@@ -63,6 +72,7 @@ void RotationKeyDecryption (char str[], int n)
 void SubstitutionCipherEncryption(char str[], char key[])
 {
     int c = 0, i, n, j = 0;
+    /*Converts to uppercase for the users string*/
     while (str[c] != '\0') 
     {
         if (str[c] >= 'a' && str[c] <= 'z')
@@ -71,7 +81,7 @@ void SubstitutionCipherEncryption(char str[], char key[])
         }
         c++;
    }
-
+    /*Converts the key to uppercase using the same method*/
     while (key[j] != '\0')
     {
         if(key[j] >= 'a' && key[j] <= 'z')
@@ -80,11 +90,15 @@ void SubstitutionCipherEncryption(char str[], char key[])
         }
         j++;
     }
+    /*Reads until the end of the string is reached*/
     for(i = 0; str[i] != '\0'; i++)
           {
+              /*For uppercase letters*/
               if(str[i] >= 'A' && str[i] <= 'Z')
               {
+                  /*Finds the key for that letter in alphabet(position in alphabet (eg a is 0, b is 1))*/
                   n = str[i] - 65;
+                  /*Takes that number and convert the string character to the corresponding numbered character in the new alphabet*/
                   str[i] = key[n];
               }
           }
@@ -96,6 +110,7 @@ void SubstitutionCipherEncryption(char str[], char key[])
 void SubstitutionCipherDecryption(char str[], char key[])
 {  
     int c = 0, i, j = 0, idk = 0, y;
+    /*Reads string and converts to uppercase*/
     while (str[c] != '\0') 
     {
         if (str[c] >= 'a' && str[c] <= 'z')
@@ -104,7 +119,7 @@ void SubstitutionCipherDecryption(char str[], char key[])
         }
         c++;
     }
-
+    /*Reads key and converts to uppercase*/
     while (key[j] != '\0')
     {
         if(key[j] >= 'a' && key[j] <= 'z')
@@ -113,17 +128,24 @@ void SubstitutionCipherDecryption(char str[], char key[])
         }
         j++;
     }
+    /*reads the whole string*/
     for(i = 0; str[i] != '\0'; i++)
     {
+      /*If that character in the sting is an uppercase letter*/
       if(str[i] >= 'A' && str[i] <= 'Z')
       {
+          /*calculates the number corresponding to that character on the new alphabet
+          by taking the temporary value idk and increasing it very time the characters dont equal*/
           for(y = 0; key[y] != '\0' && key[y] != str[i]; y++)
           {
                  idk++;
           }
+      /*Takes the placement of the character in the new alphabet and matches in with the corresponding
+      letter on the regular alphabet*/
       str[i] = idk + 65;
+      /*Reverts the temporary value back to zero to calculate the next character in the string*/
       idk = 0 ;
       }
-    }
+   }
     return;
 }
